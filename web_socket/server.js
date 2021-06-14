b58244fe2476a142e2a87702622a6843b7039241
DragosDev <barbuta.dragos12@gmail.com>
2021-06-14 20:58:17 +0300

    lecture 4

---
+++ web_socket/server.js
@@ -0,0 +1,11 @@
+var WebSocketServer = require('ws').Server
+ws = new WebSocketServer({ port:40510 })
+ws.on('connection', function(ws){
+    ws.on('message', function(message){
+        console.log('Mesaj primit de la client: ' + message)
+    })
+
+    ws.send('Asta e un mesaj din partea serverului catre client')
+
+
+})
